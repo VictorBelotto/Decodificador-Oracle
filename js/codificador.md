e3d7cd6b223b8c57dea894da22c8f5513a2e03e2
VictorBelotto <victortucunduva.vb@outlook.com>
2023-01-11 22:53:27 -0300

    Botao codificar e descodificar

--- js/codificador.md
+++ js/codificador.md
@@ -5,15 +5,15 @@ A letra "a" é convertida para "ai"
 A letra "o" é convertida para "ober"
 A letra "u" é convertida para "ufat"
 
-let letraA = "a amarela";
+let letraA = "uma frase muito louca que faça sentido e volte a ser como era antes com muitas palavras ";
 
 console.log(`Antes do Replace ${letraA}`);
 
-let letraMod = letraA.replace(/a/g, "ai");
+let letraMod = letraA.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
 
 console.log(`Depois do Replace ${letraMod}`);
 
-let devolta = letraMod.replace(/ai/g, "a")
+let devolta = letraMod.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u")
 
 console.log(`Depois do Replace de volta ${devolta}`);
 
--- js/index.js
+++ js/index.js
@@ -2,9 +2,33 @@ const textoCodificador = document.querySelector("#texto_codificador-descodificad
 
 const botaoCodificar = document.querySelector(".botao-criptografar");
 
+const botaoDescodificar = document.querySelector(".botao-descriptografar");
+
 botaoCodificar.addEventListener("click" , (event) => {event.preventDefault();
 
-    
 
+    let textoParaCodificar = textoCodificador.value;
+
+   let textoCodificado = textoParaCodificar.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+
+    console.log(`Texto a ser Codificado: ${textoParaCodificar}`)
+
+    console.log(`Texto codificado: ${textoCodificado}`)
 })
  
+
+botaoDescodificar.addEventListener("click", (event) => {event.preventDefault();
+
+    let textoParaDescodificar = textoCodificador.value;
+
+    console.log(`Texto a ser Descodificar: ${textoParaDescodificar}`)
+
+    let textoDescodificado = textoParaDescodificar.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+
+    console.log(`Texto descodificado: ${textoDescodificado}`)
+
+
+})
+
+
+/* Botao com parent node, ai usa a mesma função para codi/descodi , nele ele identifica em qual foi clicado */
