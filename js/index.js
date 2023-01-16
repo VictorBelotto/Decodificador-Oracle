03033db4c7ca62e52a100f0a245549647884f356
VictorBelotto <victortucunduva.vb@outlook.com>
2023-01-15 22:27:43 -0300

    Finalizado
    Rever o Codigo

--- js/index.js
+++ js/index.js
@@ -4,31 +4,57 @@ const botaoCodificar = document.querySelector(".botao-criptografar");
 
 const botaoDescodificar = document.querySelector(".botao-descriptografar");
 
-botaoCodificar.addEventListener("click" , (event) => {event.preventDefault();
+const botaoCopiar = document.querySelector(".botao-copiar");
+
+const mensagemResultado = document.querySelector("#mensagem-resultado");
+
+const cardBotoes = document.querySelector("#card_botoes");
+
+const textoNenhumaMensagem = document.querySelector(".texto_nenhuma-mensagem");
 
 
-    let textoParaCodificar = textoCodificador.value;
+cardBotoes.addEventListener("click" , (event) => {event.preventDefault();
 
-   let textoCodificado = textoParaCodificar.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
 
-    console.log(`Texto a ser Codificado: ${textoParaCodificar}`)
+    if(textoCodificador.value !== ""){
 
-    console.log(`Texto codificado: ${textoCodificado}`)
+        if(event.target == botaoCodificar){
+            let textoParaCodificar = textoCodificador.value;
+    
+            let textoCodificado = textoParaCodificar.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+    
+            mensagemResultado.value = textoCodificado;
+        }
+    
+        if(event.target == botaoDescodificar){
+            let textoParaDescodificar = textoCodificador.value;
+    
+            let textoDescodificado = textoParaDescodificar.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+    
+            mensagemResultado.value = textoDescodificado;
+            
+        }
+
+        textoNenhumaMensagem.classList.add("display-none")
+        mensagemResultado.classList.remove("display-none")
+        botaoCopiar.classList.remove("display-none")
+
+    
+    }else{
+        mensagemResultado.classList.add("display-none")
+        textoNenhumaMensagem.classList.remove("display-none")
+        botaoCopiar.classList.add("display-none")
+    }
+
+
+   
+
 })
  
-
-botaoDescodificar.addEventListener("click", (event) => {event.preventDefault();
-
-    let textoParaDescodificar = textoCodificador.value;
-
-    console.log(`Texto a ser Descodificar: ${textoParaDescodificar}`)
-
-    let textoDescodificado = textoParaDescodificar.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
-
-    console.log(`Texto descodificado: ${textoDescodificado}`)
-
-
+botaoCopiar.addEventListener("click", (event)=> {event.preventDefault();
+    let textoCopiado = mensagemResultado;
+    textoCopiado.select();
+    textoCopiado.setSelectionRange(0, 99999)
+    document.execCommand("copy");
 })
-
-
-/* Botao com parent node, ai usa a mesma função para codi/descodi , nele ele identifica em qual foi clicado */
+    
