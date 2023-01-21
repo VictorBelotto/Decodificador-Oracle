7dd2ff20ea2b04ded0ca84be3fd1b190e302624b
VictorBelotto <victortucunduva.vb@outlook.com>
2023-01-21 11:42:20 -0300

    Declarando Funções
    Testando Clean code

--- js/index.js
+++ js/index.js
@@ -25,7 +25,21 @@ cardBotoes.addEventListener("click" , (event) => {event.preventDefault();
     
             mensagemResultado.value = textoCodificado;
         }
-    
+
+
+
+        function logica(){
+            let array = [ [/e/g , "enter"] , [/i/g , "imes"] , [/a/g, "ai"] , [/o/g, "ober"] , [/u/g, "ufat"]]  
+
+            for(let arrays of array){
+
+            }
+        }
+
+
+
+
+        /* --------------------------------------------------------------------- */
         if(event.target == botaoDescodificar){
             let textoParaDescodificar = textoCodificador.value;
     
@@ -35,28 +49,42 @@ cardBotoes.addEventListener("click" , (event) => {event.preventDefault();
             
         }
 
-        textoNenhumaMensagem.classList.add("display-none")
-        mensagemResultado.classList.remove("display-none")
-        botaoCopiar.classList.remove("display-none")
-
+        
+        exibeResultado()
     
     }else{
-        mensagemResultado.classList.add("display-none")
-        textoNenhumaMensagem.classList.remove("display-none")
-        botaoCopiar.classList.add("display-none")
+        exibeNenhumaMensagem();
     }
-
-
-    textoCodificador.focus();
 
 })
  
 botaoCopiar.addEventListener("click", (event)=> {event.preventDefault();
+   
+    btnCopiar()
+    exibeNenhumaMensagem()
+})
+    
+
+
+
+function exibeNenhumaMensagem(){
+    textoCodificador.value = "";
+    mensagemResultado.value = "";
+    mensagemResultado.classList.add("display-none")
+    textoNenhumaMensagem.classList.remove("display-none")
+    botaoCopiar.classList.add("display-none")
+    textoCodificador.focus();
+}
+
+function exibeResultado(){
+    textoNenhumaMensagem.classList.add("display-none")
+    mensagemResultado.classList.remove("display-none")
+    botaoCopiar.classList.remove("display-none")
+}
+
+function btnCopiar(){
     let textoCopiado = mensagemResultado;
     textoCopiado.select();
     textoCopiado.setSelectionRange(0, 99999)
     document.execCommand("copy");
-
-    textoCodificador.focus();
-})
-    
+}
---
+++ js/teste.js
@@ -0,0 +1,10 @@
+let name = "Victor"
+let names = ["victor", "Ellen"]
+
+for(let nome of name){
+    console.log(nome)
+}
+
+for(let nomes of names){
+    console.log(nomes)
+}
